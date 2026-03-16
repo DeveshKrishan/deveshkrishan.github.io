4d7bcd754a4231e0fb2f8f7b4c433f66c13e521b
Devesh Krishan <deveshkrishan2003@gmail.com>
2026-03-16 12:50:47 -0700

    chore: run formatter on code

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,7 @@
-export default function Footer(){
-    return (
-        <div className="footer">
-            <p id="footer_p">© 2023 devesh krishan | personal portfolio.</p>
-        </div>
-    )
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p id="footer_p">© 2023 devesh krishan | personal portfolio.</p>
+    </div>
+  );
 }
--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,13 +1,20 @@
-function Welcome(){
-    return (
-        <div className="intro_spacing">
-            <div className="intro">
-                <p className id="name">👋 Hello! I am Devesh!</p>
-                <h1 className id="who">A full-stack software engineer with years of experience crafting wonderful projects that are scalable and usable for people! </h1>
-                <p className id="quote">"The best way to predict the future is to create it." - Abraham Lincoln</p>
-            </div>
-        </div>
-    )
+function Welcome() {
+  return (
+    <div className="intro_spacing">
+      <div className="intro">
+        <p className="intro-name" id="name">
+          👋 Hello! I am Devesh!
+        </p>
+        <h1 className="intro-title" id="who">
+          A full-stack software engineer with years of experience crafting wonderful projects that
+          are scalable and usable for people!{' '}
+        </h1>
+        <p className="intro-quote" id="quote">
+          &quot;The best way to predict the future is to create it.&quot; - Abraham Lincoln
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Welcome;
